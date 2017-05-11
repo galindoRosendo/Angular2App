5405c7935f59e1f322b7c75ea6075807653679c9
Rosendo Galindo <rosendo_galindo@hotmail.com>
2017-05-11 02:02:52 -0500

    Added ngIf, ngClass and Click event

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from 'angular2/core';
-
+import {OPelicula} from "./models/peliculas"
 @Component({
     selector:'mi-app',
-    template:'<h1>Hola mundo</h1>'
+    templateUrl:'app/views/peliculas.html',
+    styleUrls:['../assets/css/style.css']
 })
 
-export class AppComponent{}
+export class AppComponent{
+
+    public TituloPagina:string = 'Mejores Peliculas';
+    public MostrarDatos:boolean;
+
+    Pelicula:OPelicula;
+    constructor(){
+        this.MostrarDatos=false;
+        this.Pelicula= new OPelicula(1,"Batman v Superman","Rosendo Galindo", 2017);
+    }
+
+    onShowHide(value){
+        this.MostrarDatos=value;
+    }
+}
---
+++ app/models/peliculas.ts
@@ -0,0 +1,9 @@
+export class OPelicula{
+    constructor(
+    public IdPelicula:number,
+    public Titulo:string,
+    public Director:string,
+    public Anio:number
+    ){}
+
+}
