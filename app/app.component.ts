59b88adcc13e4b62f3331411cb142436f65545ce
Rosendo Galindo <rosendo_galindo@hotmail.com>
2017-05-12 00:22:28 -0500

    basic routing and routing components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from 'angular2/core';
-import {PeliculasListComponent} from "./components/peliculas-list.component";
-import {PeliculasFooterComponent} from "./components/peliculas-footer.component";
+import { PeliculasListComponent } from "./components/peliculas-list.component";
+import { PeliculasFooterComponent } from "./components/peliculas-footer.component";
+import { ContactoComponent } from "./components/contacto.component";
+
+import {ROUTER_DIRECTIVES, RouteConfig, Router} from "angular2/router";
 
 @Component({
     selector:'mi-app',
     templateUrl:'app/views/peliculas.html',
     directives:[
         PeliculasListComponent,
-        PeliculasFooterComponent
+        PeliculasFooterComponent,
+        ROUTER_DIRECTIVES,
+        ContactoComponent
     ]
 })
+
+@RouteConfig([
+    { path:"/peliculas", name:"Peliculas", component:PeliculasListComponent, useAsDefault:true },
+    { path:"/contacto", name:"Contacto", component:ContactoComponent}
+])
 
 export class AppComponent{
 
---
+++ app/components/contacto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector:'contacto',
+    templateUrl:'app/views/contacto.html'
+})
+
+export class ContactoComponent{
+
+    public TituloPagina:string = 'Contacto';
+}
