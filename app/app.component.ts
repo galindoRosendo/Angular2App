05d56813412dbbac390da09cb8574718c57c6c10
Rosendo Galindo <rosendo_galindo@hotmail.com>
2017-05-11 22:37:11 -0500

    ngFor, Array of objects, best practices

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from 'angular2/core';
-import {OPelicula} from "./models/peliculas"
+import {OPelicula} from "./models/peliculas";
+
 @Component({
     selector:'mi-app',
     templateUrl:'app/views/peliculas.html',
@@ -10,11 +11,20 @@ export class AppComponent{
 
     public TituloPagina:string = 'Mejores Peliculas';
     public MostrarDatos:boolean;
-
+    public Peliculas:Array<OPelicula>;
     Pelicula:OPelicula;
+    
     constructor(){
         this.MostrarDatos=false;
         this.Pelicula= new OPelicula(1,"Batman v Superman","Rosendo Galindo", 2017);
+        
+        this.Peliculas=[
+            new OPelicula(1,"WonderWoman: The Amazon Ladys","Rosendo Galindo", 2017),
+            new OPelicula(1,"SuperMan: Man of steel","Rosendo Galindo", 2017),
+            new OPelicula(1,"Flash: Time travel","Rosendo Galindo", 2017),
+            new OPelicula(1,"AquaMan: The DeepSea","Rosendo Galindo",null )
+        ];
+        
         this.debug();
     }
 
